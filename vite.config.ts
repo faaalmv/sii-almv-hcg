e2ecc1ae8955ce8fa7bc787227e8e3b49b9a365d
faaalmv <faaalmv@gmail.com>
2025-09-27 19:53:44 -0600

    unir

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    // Carga las variables de entorno de .env.local
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0', // Permite el acceso desde otras IPs en la red local si es necesario
+      },
+      plugins: [react()],
+      define: {
+        // Exponer la URL base del API a la aplicación React.
+        'import.meta.env.VITE_API_URL_BASE': JSON.stringify(env.VITE_API_URL_BASE || 'http://localhost:8080/api/v1'),
+      },
+    };
+});
