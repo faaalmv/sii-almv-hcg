32308ea6b07a9a8c92036682b14e082010310288
faaalmv <faaalmv@gmail.com>
2025-09-28 03:31:28 +0000

    typy

--- src/modules/salidas/types.ts
+++ src/modules/salidas/types.ts
@@ -1,16 +1,23 @@
 
+export interface Lote {
+  id: string;
+  fechaCaducidad: Date;
+  cantidad: number;
+}
+
 export interface ItemData {
     codigo: number;
     descripcion: string;
     um: string;
     maximo: number;
+    lotes: Lote[];
 }
 
 export interface OrderItem {
     id: string;
     codigo: string;
     descripcion: string;
-    um: string;
+um: string;
     cantidadPedida: string;
     cantidadSurtida: string;
     observaciones: string;
