b65394e8c0095e253c317c77110f7afaf8d03173
faaalmv <faaalmv@gmail.com>
2025-09-27 20:48:25 -0600

    agregar exportación por defecto de iconos para facilitar su uso

--- src/common/icons/Icon.tsx
+++ src/common/icons/Icon.tsx
@@ -118,3 +118,28 @@ export const MagnifyingGlassIcon: React.FC<IconProps> = ({ className = 'w-6 h-6'
         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
     </svg>
 );
+
+// Default export helper: agrupa los iconos para permitir `import Icon from '.../icons/Icon'` y uso como `Icon.Upload`.
+const Icon: Record<string, React.FC<IconProps>> = {
+    Upload: UploadIcon,
+    CheckCircle: CheckCircleIcon,
+    XCircle: XCircleIcon,
+    Eye: EyeIcon,
+    ChevronDown: ChevronDownIcon,
+    DocumentText: DocumentTextIcon,
+    Clock: ClockIcon,
+    Check: CheckIcon,
+    X: XIcon,
+    BuildingOffice: BuildingOfficeIcon,
+    UserCircle: UserCircleIcon,
+    CalendarDays: CalendarDaysIcon,
+    InformationCircle: InformationCircleIcon,
+    Link: LinkIcon,
+    CurrencyDollar: CurrencyDollarIcon,
+    ExclamationTriangle: ExclamationTriangleIcon,
+    ArchiveBoxXMark: ArchiveBoxXMarkIcon,
+    DocumentPlus: DocumentPlusIcon,
+    MagnifyingGlass: MagnifyingGlassIcon,
+};
+
+export default Icon;
